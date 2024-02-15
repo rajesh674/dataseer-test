e0e628c14ac2f739fca60c6c5ddc154e8d8551de
rajesh674 <mishra.rajesh674@gmail.com>
2024-02-15 13:55:50 +0530

    test project

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+type Props = {
+  children?: React.ReactNode;
+  data: {
+    id: number;
+    uuid: string;
+    title: string;
+    subtitle: string;
+    updatedAt: string;
+  };
+};
+
+const Card = ({ data }: Props) => {
+  return (
+    <div className="shadow-lg flex w-full cursor-pointer">
+      <main className="py-7 px-5 rounded-r-md w-full bg-white">
+      <h1 className="font-bold text-xl">{data.title}</h1>
+        <span className="flex flex-row justify-between">
+          <h4 className="uppercase font-normal">{data.subtitle}</h4>
+          <p>{data.updatedAt}</p>
+        </span>
+      </main>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/Incorporate/index.tsx
@@ -0,0 +1,168 @@
+import Card from "../Card/index";
+import List from "../List/index";
+import {
+  DragDropContext,
+  Draggable,
+  DraggableProvided,
+  DraggableStateSnapshot,
+} from "react-beautiful-dnd";
+import { useState } from "react";
+
+const catgegory = ['available', 'panding', 'assigned', 'complete']
+export type inputDataType = {
+  id: number;
+  uuid: string;
+  title: string;
+  subtitle: string;
+  updatedAt: string;
+}
+const Incorporate = () => {
+  const itemsNormal = {
+    available: [
+      {
+        id: 1,
+        uuid: "52f9df20-9393-4c4d-b72c-7bfa4398a4477",
+        title: "Task List",
+        subtitle: "Lorem Ipsum is simply dummy",
+        updatedAt: "6 days ago",
+      },
+      {
+        id: 2,
+        uuid: "52f9df20-9393-4c4d-b72c-7bfa4398a448",
+        title: "Task List 1",
+        subtitle: "Lorem Ipsum is simply dummy",
+        updatedAt: "6 days ago",
+      },
+      {
+        id: 3,
+        uuid: "52f9df20-9393-4c4d-b72c-7bfa4398a449",
+        title: "Task List 2",
+        subtitle: "Lorem Ipsum is simply dummy",
+        updatedAt: "6 days ago",
+      },
+    ],
+
+    assigned: [
+      {
+        id: 5,
+        uuid: "52f9df20-9393-4c4d-b72c-7bfa4398a450",
+        title: "Task List 3",
+        subtitle: "Lorem Ipsum is simply dummy",
+        updatedAt: "6 days ago",
+      },
+      {
+        id: 6,
+        uuid: "52f9df20-9393-4c4d-b72c-7bfa4398a451",
+        title: "Task List 4",
+        subtitle: "Lorem Ipsum is simply dummy",
+        updatedAt: "2 days ago",
+      },
+    ],
+    panding: [
+      {
+        id: 7,
+        uuid: "52f9df20-9393-4c4d-b72c-7bfa4398a452",
+        title: "Task List 7",
+        subtitle: "Lorem Ipsum is simply dummy",
+        updatedAt: "6 days ago",
+      },
+      {
+        id: 8,
+        uuid: "52f9df20-9393-4c4d-b72c-7bfa4398a453",
+        title: "Task List 8",
+        subtitle: "Lorem Ipsum is simply dummy",
+        updatedAt: "2 days ago",
+      },
+    ],
+    complete: [
+      {
+        id: 9,
+        uuid: "52f9df20-9393-4c4d-b72c-7bfa4398a454",
+        title: "Task List 9",
+        subtitle: "Lorem Ipsum is simply dummy",
+        updatedAt: "6 days ago",
+      },
+      {
+        id: 10,
+        uuid: "52f9df20-9393-4c4d-b72c-7bfa4398a455",
+        title: "Task List 10",
+        subtitle: "Lorem Ipsum is simply dummy",
+        updatedAt: "2 days ago",
+      },
+    ],
+  };
+
+  const [items, setItems] = useState(itemsNormal);
+  const { available, panding, assigned, complete } = items;
+  let localData = JSON.parse(localStorage.getItem('data') as 'string | null')
+  let updataavailable = localData === null ? [...available] : [...available,...localData];
+
+  const removeFromList = (list: any, index: any) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(index, 1);
+    return [removed, result];
+  };
+
+  const addToList = (list: any, index: any, element: any) => {
+    const result = Array.from(list);
+    result.splice(index, 0, element);
+    return result;
+  };
+
+  const onDragEnd = (result: any) => {
+    if (!result.destination) {
+      console.log(result);
+      return;
+    }
+    const listCopy: any = { ...items };
+    const sourceList = listCopy[result.source.droppableId];
+    const [removedElement, newSourceList] = removeFromList(
+      sourceList,
+      result.source.index
+    );
+    listCopy[result.source.droppableId] = newSourceList;
+
+    const destinationList = listCopy[result.destination.droppableId];
+    listCopy[result.destination.droppableId] = addToList(
+      destinationList,
+      result.destination.index,
+      removedElement
+    );
+    setItems(listCopy);
+  };
+  
+
+
+  return (
+    <>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <div className="flex p-12">
+          {catgegory.map((el, i) => {
+            let titleName = `${el.charAt(0).toUpperCase()}${el.slice(1)} Tasks`;
+            let data = el === 'available' ? updataavailable : el === 'panding' ? panding : el === 'assigned' ? assigned : complete
+            return <List key={i} title={titleName} onDragEnd={onDragEnd} name={el}>
+              {data?.map((item, index) => (
+                <Draggable key={item.id} draggableId={item.id + ""} index={index}>
+                  {(
+                    provided: DraggableProvided | any,
+                    snapshot: DraggableStateSnapshot
+                  ) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        <Card data={item} />
+                      </div>
+                  )}
+                </Draggable>
+              ))}
+            </List>
+          })}
+        </div>
+      </DragDropContext>
+    </>
+  );
+};
+
+export default Incorporate;
